bb0b35e91e0bd0283afe2afa2a8547357aae5161
Ahmed Awadallah <aawadall@ualberta.ca>
2023-03-14 17:12:29 -0400

    add dependencies for token

--- sim/routes/logout.js
+++ sim/routes/logout.js
@@ -6,4 +6,8 @@ const controller = require('../controllers/logoutController');
 // GET /logout
 router.get('/', controller.logout);
 
+// POST /logout
+router.post('/', controller.logout);
+
+
 module.exports = router;
